6f99045d2260ca44a90496ae2240f68f9eff0767
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-09-13 18:54:07 +0200

    Add support for initialisation function to skooma

--- skooma.js
+++ skooma.js
@@ -40,6 +40,8 @@ const parseArgs = (element, before, ...args) => {
 	for (let arg of args)
 		if (typeof arg == "string" || typeof arg == "number")
 			element.insertBefore(document.createTextNode(arg), before)
+		else if (typeof arg == "function")
+			arg(element)
 		else if ("nodeName" in arg)
 			element.insertBefore(arg, before)
 		else if (arg.constructor?.name === "Promise")
